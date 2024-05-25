af2f831088cf930a6210a05d864075e3f710ffcf
Hansel Tritama <hansel.tritama@gendigital.com>
2024-05-24 19:15:06 -0700

    adding animation, delegate properties, etc

--- app/src/main/java/com/example/composebasics/MainActivity.kt
+++ app/src/main/java/com/example/composebasics/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.composebasics
 
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,8 +21,12 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,9 +37,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeBasicsTheme {
-                GreetingsView()
+                MyApp()
             }
         }
+    }
+}
+
+@Composable
+fun MyApp() {
+    // `by` is a delegate value --> Single Responsibility Principle
+    // so, `.value` is no longer needed!
+    // source: https://medium.com/@emrememil/delegation-in-kotlin-delegated-properties-efedb595b3aa
+
+    // rememberSaveable --> to remember when there's config change, such as dark more, rotation, etc
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClick = { shouldShowOnboarding = false })
+    } else {
+        GreetingsView()
     }
 }
 
@@ -57,8 +82,14 @@ fun GreetingsView(names: List<String> = listOf("World", "Compose"), modifier: Mo
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     // remember is a keyword that we use to prevent it from re-initiating when
     // Greeting view is being re-composed
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded) 48.dp else 0.dp,
+        animationSpec = tween(
+            durationMillis = 500
+        )
+    )
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier
@@ -82,11 +113,37 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             OutlinedButton(
                 colors = ButtonDefaults.outlinedButtonColors(containerColor = MaterialTheme.colorScheme.background),
                 border = BorderStroke(2.dp, MaterialTheme.colorScheme.inverseSurface),
-                onClick = { expanded.value = !expanded.value }
+                onClick = { expanded = !expanded }
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClick: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClick
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeBasicsTheme {
+        OnboardingScreen(onContinueClick = {})
     }
 }
 
